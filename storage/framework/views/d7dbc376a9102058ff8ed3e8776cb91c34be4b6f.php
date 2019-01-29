df30bf56c5bb50d0510afc8be5b0c3c9c12d6ee8
ahammadjaffer <47079045+ahammadjaffer@users.noreply.github.com>
2019-01-29 13:22:23 +0530

    Add files via upload

--- storage/framework/views/d7dbc376a9102058ff8ed3e8776cb91c34be4b6f.php
+++ storage/framework/views/d7dbc376a9102058ff8ed3e8776cb91c34be4b6f.php
@@ -88,10 +88,10 @@
           Menu
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="/userLogout">Logout</a>
           <a class="dropdown-item" href="#">Another action</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="/#">Another action 2</a>
         </div>
       </li>
     </ul>
@@ -142,7 +142,6 @@
 </div>
 
 <div class="clearBoth"></div>
-
 
 </body>
 </html>
